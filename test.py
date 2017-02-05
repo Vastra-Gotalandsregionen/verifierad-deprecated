017b9d6f6c9debc7c95d263869b4a0883bf79949
Marcus Österberg <marcus@webbfunktion.com>
2017-02-05 01:37:00 +0100

    Fixed the timeouts (I think)

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 import sys
+import socket
 import urllib
 from urllib.request import Request, urlopen
 from urllib.error import HTTPError
@@ -7,7 +8,7 @@ import _privatekeys as privatekeys
 
 ### variable defaults
 user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-timeout_in_seconds = 90 
+timeout_in_seconds = 30 
 
 def mobileFriendlyCheck(request_url, api_key):
 	"""	Example of Python talking to Google Search Console URL Testing Tools API.
@@ -41,8 +42,11 @@ def httpStatusCodeCheck(url, simulate_404 = False):
 
 		return a.code	# returns the status code for HTTP, 200 for Ok, 404 for Not found, etc
 	except urllib.error.URLError as e:
-		print(e.code)  
-		pass					# I'll let it pass, this time!
+		return e.code
+	except socket.timeout:
+		return '522'	# not sure which error code to choose, but 520-522 seems reasonable according to https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+	except:
+		return '520'
 
 def httpRequestGetContent(url):
 	"""Trying to fetch the response content
@@ -64,5 +68,5 @@ If file is executed on itself then call definition mobileFriendlyCheck()
 if __name__ == '__main__':
 	print('Initiating definition "httpStatusCodeCheck()"')
 	#print(httpRequestGetContent("http://gp43789435.se"))
-	print(httpStatusCodeCheck("http://vgregion.se"))
+	print(httpStatusCodeCheck("http://www.ersattningsnamnden.se/"))
 	#mobileFriendlyCheck('http://www.varberg.se/byggabomiljo/fastigheterlantmateriochkartor/kartor/saanvanderdudigitalavarbergskartan.4.7b99eb041540f79d87d77155.html', privatekeys.googleMobileFriendlyApiKey)
