6e21b60df70496f81df3882b0d1c614550e780a2
Thomas Svenson <thomas@tsvenson.com>
2017-05-22 23:03:28 +0200

    Improves documentation about how to get keys and copy file for use.

--- _privatekeys_CHANGE-ME.py
+++ _privatekeys_CHANGE-ME.py
@@ -1,14 +1,22 @@
-# get your Google API keys at https://developers.google.com/webmaster-tools/search-console-api/v1/configure
+"""
+To be able to access the Google and Twitter tools, you need to have API access keys.
+
+To manage and get your Google API keys:
+    https://developers.google.com/webmaster-tools/search-console-api/v1/configure
+
+To manage and get your Twitter API keys, follow the instructions at:
+    https://dev.twitter.com/oauth/overview/application-owner-access-tokens
+
+Then copy and rename this file to '_privatekeys.py' and replace the 'CHANGE-ME' strings
+with the relevant API keys.
+"""
+
+# Google API keys
 googlePageSpeedApiKey = "CHANGE-ME"
 googleMobileFriendlyApiKey = "CHANGE-ME"
 
+# Twitter API keys
 twitterConsumerKey = "CHANGE-ME"
 twitterConsumerSecret = "CHANGE-ME"
 twitterAccessToken = "CHANGE-ME"
 twitterAccessTokenSecret = "CHANGE-ME"
-
-# mySqlServer = ""
-# mySqlDbName = ""
-# mySqlUid = ""
-# mySqlPw = ""
-# mySqlTable = ""
